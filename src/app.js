c0884da6577ffd2079208d1c299059f8b9a78c40
Danilo Cândido <danilod07@gmail.com>
2018-10-10 16:02:42 -0300

    button and square

--- src/app.js
+++ src/app.js
@@ -2,11 +2,28 @@
 
 import React from 'react'
 
+import Button from './button'
+import Square from './square'
+
 class App extends React.Component {
+  constructor () {
+    super()
+    this.state = { color: 'green' }
+  }
+
   render () {
     return (
-      <div className='container' >
-        My App
+      <div>
+        <Square color={this.state.color} />
+
+        {['green', 'red', 'blue'].map((color, index) => (
+          <Button
+            key={index}
+            handleClick={() => this.setState({ color })}>
+            { color }
+          </Button>
+        ))}
+
       </div>
     )
   }
